71287ca2efbd6e39c25f6367968ec01504697a14
Andrew <zoitanu.andrei@yahoo.com>
2019-03-05 17:05:57 +0100

    bump

--- routes/apicms.js
+++ routes/apicms.js
@@ -10,6 +10,7 @@ const uuidv1 = require('uuid/v1');
 var generator = require('generate-password');
 const bcrypt = require('bcrypt-nodejs');
 const nodemailer = require('nodemailer');
+const pause = require('../pause');
 
 const httpStatus = [100,101,102,200,201,202,203,204,205,206,207,208,226,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,421,422,423,424,426,428,429,431,444,451,499,500,501,502,503,504,505,506,507,508,510,511,59];
 
@@ -137,7 +138,7 @@ router.get('/apis/:project/:api', (req, res) => {
         if(active_version === ""){
           res.send("Thsi API doesn't have an active version.");
         } else{
-          setTimeout(() => {
+          pause(delay).then(() => {
             if (httpStatus.includes(status)){
               res.status(status);
             }
@@ -177,8 +178,7 @@ router.get('/apis/:project/:api', (req, res) => {
               res.status(404);
               res.send(err);
             }
-          }, delay);
-
+          });
         }
 
     }
@@ -203,7 +203,7 @@ router.post('/apis/:project/:api', (req, res) => {
         if(active_version === ""){
           res.send("Thsi API doesn't have an active version.");
         } else{
-          setTimeout(() => {
+          pause(delay).then(() => {
             if (httpStatus.includes(status)){
               res.status(status);
             }
@@ -241,12 +241,10 @@ router.post('/apis/:project/:api', (req, res) => {
             }
             catch(err) {
               res.status(404);
-              res.send("Thsi API doesn't exist.");
+              res.send(err);
             }
-          }, delay);
-
+          });
         }
-
     }
   });
 });
